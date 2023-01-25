753f166d72e2a2f78274807b9f0179f592c38102
muchendu221 <muchendu22@gmail.com>
2023-01-25 00:44:42 -0800

    hot fix

--- _printf.c
+++ _printf.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-*_printf - Prints a formatted string
-*
-* @format: a string to be printed
-* Return: count of characters printed
-*/
+ * _printf - Receives the main string and all the necessary parameters to
+ * print a formated string
+ * @format: A string containing all the desired characters
+ * Return: A total count of the characters printed
+ */
 int _printf(const char *format, ...)
 {
 	int printed_chars;
@@ -13,14 +13,25 @@ int _printf(const char *format, ...)
 		{"c", print_char},
 		{"s", print_string},
 		{"%", print_percent},
+		{"d", print_integer},
+		{"i", print_integer},
+		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
+		{"u", unsigned_integer},
+		{"o", print_octal},
+		{"x", print_hex},
+		{"X", print_heX},
 		{NULL, NULL}
 	};
 	va_list arg_list;
 
 	if (format == NULL)
 		return (-1);
+
 	va_start(arg_list, format);
+	/*Calling parser function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
-		return (printed_chars);
+	return (printed_chars);
 }
