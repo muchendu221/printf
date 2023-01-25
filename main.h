d0fec79606b9f5e379269c55bac233927c7281ba
muchendu221 <muchendu22@gmail.com>
2023-01-25 00:39:45 -0800

    hot fix

--- main.h
+++ main.h
@@ -1,16 +1,16 @@
-#ifndef PRINTF
-#define PRINTF
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdarg.h>
 
 /**
- *struct convert - defines a structure for symbols and functions
- *
- *@sym: The operator
- * @f: The function
- */
+* struct convert - defines a structure for symbols and functions
+*
+* @sym: The operator
+* @f: The function associated
+*/
 struct convert
 {
 	char *sym;
@@ -21,17 +21,26 @@ typedef struct convert conver_t;
 /*Main functions*/
 int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ...);
-int _putchar(char c);
+int _putchar(char);
 int print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
+int print_integer(va_list);
+int print_number(va_list);
+int print_binary(va_list);
+int print_reversed(va_list arg);
+int rot13(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 
-/* helper functions*/
+/*Helper functions*/
 unsigned int base_len(unsigned int, int);
 char *rev_string(char *);
 void write_base(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
-int print_unsigned_number(unsigned int);
+int print_unsgined_number(unsigned int);
 
 
 #endif
